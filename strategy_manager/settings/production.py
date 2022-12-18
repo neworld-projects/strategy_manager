0e1183c14aeed0910163a37d2fce49ecbb7cf551
ali rezazadeh <lirezazadeh77@gmail.com>
2022-12-19 00:56:14 +0330

    initial project

---
+++ strategy_manager/settings/production.py
@@ -0,0 +1,12 @@
+from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default=''),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default=''),
+        'PORT': config('DB_PORT', default=''),
+    }
+}
