338321880f3c3fbb95ea2cdb129ce199ac4bebf9
ali rezazadeh <lirezazadeh77@gmail.com>
2022-12-23 14:12:28 +0330

    add retry config

--- strategy/tasks/third_party.py
+++ strategy/tasks/third_party.py
@@ -5,7 +5,9 @@ from helpers.send_to_third_party import third_party_call
 
 
 @shared_task(
-    name=settings.THIRD_PARTY_MANAGER_TASK
+    name=settings.THIRD_PARTY_MANAGER_TASK,
+    autoretry_for=(Exception, ),
+    max_retries=3
 )
 def third_party_manager(imessage: dict, target: str, message_mode: str = 'html', **kwargs):
     third_party_call(imessage, target, message_mode='html', **kwargs)
