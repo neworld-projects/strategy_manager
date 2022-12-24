d23c5126185b9ea9a447c6c7c58a6cf3db2c863d
alirezazadeh77 <69978363+alirezazadeh77@users.noreply.github.com>
2022-12-24 18:45:48 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [[ $(cat /etc/hostname) == celery ]]; then
     /usr/local/bin/celery -A strategy_manager worker -l info -Q celery --concurrency=1 -n celery@najva
 fi
 if [[ $(cat /etc/hostname) == celery-beat ]]; then
-   /usr/local/bin/celery -A strategy_manager beat -l info -S django
+   /usr/local/bin/celery -A strategy_manager beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 fi
 if [[ $(cat /etc/hostname) == tradingview-strategy-check ]]; then
    /usr/local/bin/celery -A strategy_manager worker -l info -Q tradingview_strategy_check --concurrency=1 -n tradingview_strategy_check@neworld
