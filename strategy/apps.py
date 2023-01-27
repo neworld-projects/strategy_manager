2ee615826a0b06b8193d0155138559d884f4b572
ali <lirezazadeh77@gmail.com>
2023-01-28 01:28:28 +0330

    fix cron signal

--- strategy/apps.py
+++ strategy/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class StrategyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'strategy'
+
     def ready(self):
         from . import signals
