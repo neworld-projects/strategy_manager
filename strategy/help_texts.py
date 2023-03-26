0b876ffa448d0bc94c27702a9153b4c1adf4f9ba
alirezazadeh77 <69978363+alirezazadeh77@users.noreply.github.com>
2023-03-26 13:34:59 +0330

    Update strategy/help_texts.py

--- strategy/help_texts.py
+++ strategy/help_texts.py
@@ -5,5 +5,5 @@ from strategy.enums import PositionKeys
 strategy_model_help_text = \
     'you can add bellow keys\n  {}\nuse like this format {} \nif has many use number in key'.format(
         "\n   ".join(PositionKeys.values),
-        json.dumps({'1': "open_position_value"})
+        json.dumps({'0': "open_position_value"})
     )
