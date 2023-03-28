529d141765b2941deab322f0c0fca64a800bbae6
ali <lirezazadeh77@gmail.com>
2023-03-28 15:40:54 +0330

    task failure handler

--- strategy_manager/celery.py
+++ strategy_manager/celery.py
@@ -1,14 +1,21 @@
+import logging
 import os
 
 from celery import Celery
 from celery.schedules import crontab
 from celery.signals import setup_logging
+from celery.signals import task_failure
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chapar.settings')
 
 app = Celery('strategy_manager')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+@task_failure.connect
+def task_failure_handler(sender=None, exception=None, args=None, **kwargs):
+    logging.info(f'{sender.name} {exception}')
 
 
 @setup_logging.connect
